47df8b934bf8b68861079dbb6a34b380aec99c4d
Rahulll066 <rahulsainadhmail@gmail.com>
2026-01-27 11:06:21 +0530

    Initialized next.js project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col font-extrabold items-center justify-center p-24">
+      Blockchain Wallet Generator
+    </div>
+  );
+}
